feb6f703912348ed4e7b4787105760d6ca7ec5d2
Przemyslaw Pajak <office@fearlessspider.com>
2023-04-20 15:13:16 +0200

    feat(app): add earmark, dialixir, markdown
    add earmark, dialixir, markdown
    
    Signed-off-by: Przemyslaw Pajak

--- config/dev.exs
+++ config/dev.exs
@@ -60,7 +60,7 @@ config :nieustraszony, NieustraszonyWeb.Endpoint,
       ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
       ~r"lib/nieustraszony_web/(live|views)/.*(ex)$",
-      ~r"lib/nieustraszony_web/templates/.*(eex)$"
+      ~r"lib/nieustraszony_web/templates/.*(eex|md)$"
     ]
   ]
 
--- mix.exs
+++ mix.exs
@@ -48,7 +48,13 @@ defmodule Nieustraszony.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:earmark, "~> 1.4"},
+      {:phoenix_markdown, "~> 1.0"},
+      {:phoenix_integration, "~> 0.8", only: :test},
+      {:sobelow, "~> 0.12", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.3", only: [:dev], runtime: false}
     ]
   end
 
