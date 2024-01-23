f631973ebb861afcd4f7d4ae4f6d7874217b8b28
Przemysław Pająk <office@fearlessspider.com>
2024-01-23 15:51:13 +0100

    test(page): add test for elixir, hacking, postresql page
    add test for elixir, hacking, postresql page
    
    Signed-off-by: Przemysław Pająk

--- test/nieustraszony_web/controllers/page_controller_test.exs
+++ test/nieustraszony_web/controllers/page_controller_test.exs
@@ -3,6 +3,21 @@ defmodule NieustraszonyWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Nieustraszony"
+  end
+
+  test "GET /elixir", %{conn: conn} do
+    conn = get(conn, "/elixir")
+    assert html_response(conn, 200) =~ "Nieustraszony"
+  end
+
+  test "GET /hacking", %{conn: conn} do
+    conn = get(conn, "/hacking")
+    assert html_response(conn, 200) =~ "Hacking"
+  end
+
+  test "GET /postgresql", %{conn: conn} do
+    conn = get(conn, "/postgresql")
+    assert html_response(conn, 200) =~ "PostgreSQL"
   end
 end
